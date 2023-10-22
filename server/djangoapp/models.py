e793e2ff756938dd2510dfe23b33a46c11ef7c83
Samuel <samuelilunga@rocketmail.com>
2023-10-21 22:32:54 -0400

    Week3 Checkpoint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -60,6 +60,46 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+
+    def __init__(self, address, city, id, state, lat, long, full_name, short_name, st, zip):
+        # Dealer id
+        self.id = id
+        # Dealer city
+        self.city = city
+        #Dealer State(full)
+        self.state = state
+        # Dealer address
+        self.address = address
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+        #Dealer short name
+        self.short_name = short_name
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.id = id
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+
+        def __str__(self):
+            return "Reviewer name: " + self.name
